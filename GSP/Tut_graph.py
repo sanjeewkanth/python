dbe3d63a33165be82d60bb1fd7f053e9acc9ee77
sanjeewkanth <sanjeewkanth@entgra.io>
2023-02-26 13:54:03 +0530

    GSP tutorial

---
+++ GSP/Tut_graph.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pygsp import graphs, filters, plotting
+
+plotting.BACKEND = 'matplotlib'
+plt.rcParams['figure.figsize'] = (10, 5)
+
+
+W = np.array([[0, 1, 1, 0, 0],
+ [1, 0, 1, 0, 1],
+ [1, 1, 0, 1, 0],
+ [0, 0, 1, 0, 1],
+ [0, 1, 0, 1, 0]])
+
+
+W = W + W.T
+print(W)
+np.fill_diagonal(W, 0)
+G = graphs.Graph(W)
+#print('{} nodes, {} edges'.format(G.N, G.Ne))
+G.set_coordinates('ring2D')
+G.plot()
